5cc570afdb3f6f99396d05d22882f9edd10ae8b5
Marcus Baer <marcus.baer@digitaspixelpark.com>
2026-02-11 11:25:54 +0100

    Implement prompt handling and instruction registration in HelloTools

--- src/hello.ts
+++ src/hello.ts
@@ -13,8 +13,15 @@ interface Tool {
   handle: Function;
 }
 
+interface Prompt {
+  name: string;
+  handle: Function;
+  argsSchema?: ZodRawShapeCompat | undefined;
+}
+
 export class HelloTools {
   constructor(private server: McpServer) {
+    this.registerInstructions(server);
     this.registerTools(server);
   }
 
@@ -27,6 +34,39 @@ export class HelloTools {
         },
       ],
     };
+  }
+
+  private async promptHandle(args: any) {
+    return {
+      messages: [
+        {
+          role: "user",
+          content: {
+            type: "text",
+            text: `You are now the personal assistant of the user. 
+                 RULES:
+                 1. if no name has been provided so far, ask for it
+                 2. if the user has provided a name, address him with his name ${args.name}`,
+          },
+        },
+      ],
+    };
+  }
+
+  private async registerInstructions(server: McpServer) {
+    const instructions: Prompt[] = [
+      {
+        name: "hello-instruction",
+        argsSchema: {
+          name: z.string().describe("Name of a person"),
+        },
+        handle: this.promptHandle,
+      },
+    ];
+
+    instructions.forEach((instruction) => {
+      this.addPrompt(server, instruction);
+    });
   }
 
   private async registerTools(server: McpServer) {
@@ -54,6 +94,19 @@ export class HelloTools {
     server.registerTool(
       name,
       { description, inputSchema, outputSchema },
+      handle as any,
+    );
+  }
+
+  private async addPrompt(
+    server: McpServer,
+    { name, argsSchema, handle }: Prompt,
+  ) {
+    server.registerPrompt(
+      name,
+      {
+        argsSchema,
+      },
       handle as any,
     );
   }
